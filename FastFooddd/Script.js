fc1dcee97b78a4797f97cd846267a1ddde3c63a8
haovo2002 <125528167+haovo2002@users.noreply.github.com>
2023-06-09 18:11:04 +0700

    Add files via upload

---
+++ FastFooddd/Script.js
@@ -0,0 +1,8 @@
+// alert('Welcome to Fast Food ');
+var dangnhap=document.querySelector('.dangnhap');
+var formdn=document.querySelector('.dn');
+dangnhap.onclick=function(){
+    console.log('ok');
+    formdn.classList.add('active');
+}
+
